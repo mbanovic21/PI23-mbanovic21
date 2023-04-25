d40160c219a9143ea278895821e8dd32ac6c69d4
isimic21 <isimic21@foi.hr>
2023-04-25 17:49:02 +0200

    Dorađena forma za login

--- EvaluationManagerG04/frmLogin.cs
+++ EvaluationManagerG04/frmLogin.cs
@@ -9,9 +9,17 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace EvaluationManagerG04 {
-    public partial class Form1 : Form {
-        public Form1() {
+    public partial class frmLogin : Form {
+        public frmLogin() {
             InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e) {
+
+        }
+
+        private void txtPassword_TextChanged(object sender, EventArgs e) {
+
         }
     }
 }
